24a947fc2f5b9c8fb761053b5541d2cbeebfd01b
Zachary Kleine <zach.kleine18@gmail.com>
2023-06-02 10:21:25 -0400

    added function to merge files to excel

--- bbexposure.ps1
+++ bbexposure.ps1
@@ -1,8 +1,15 @@
+Param(
+    [CmdletBinding()]
+    [Parameter(Mandatory=$False)][String]$BBDir = "G:\My Drive\Fantasy Football\BestBall",
+    [Parameter(Mandatory=$False)][String]$UD_csv = "UD_Exposure_Raw.csv",
+    [Parameter(Mandatory=$False)][String]$DK_csv = "DK_Exposure_Raw.csv"
+)
+Import-Module -Name ImportExcel
 Function Get-UDExposure {
     Param(
         [CmdletBinding()]
-        [Parameter(Mandatory=$False)][String]$BBDir = "G:\My Drive\Fantasy Football\BestBall",
-        [Parameter(Mandatory=$False)][String]$UD_csv = "Underdog_Exposure.csv"
+        [Parameter(Mandatory=$False)][String]$BBDir,
+        [Parameter(Mandatory=$False)][String]$UD_csv
     )
     Push-Location -Path $BBDir
     $FilePath = Join-Path -Path $BBDir -ChildPath $UD_csv
@@ -19,4 +26,38 @@ Function Get-UDExposure {
     $PlayerCounts | Select-Object -Property Name,Exposure | Export-Csv -Path "UD_Exposure.csv" -NoTypeInformation -Force
     Pop-Location
 }
-Get-UDExposure
+Function Add-Files {
+    Param(
+        [CmdletBinding()]
+        [Parameter(Mandatory=$False)][String]$BBDir,
+        [Parameter(Mandatory=$False)][String]$UD_Exposure
+        #[Parameter(Mandatory=$False)][String]$DK_Exposure
+    )
+    $ExcelFile = "BestBallBuddy.xlsx"
+    $KerraneRankings = "Best-Ball---Ranks.csv"
+    $DK_Adp = "DkPreDraftRankings.csv"
+
+    Push-Location -Path $BBDir
+    
+    $PlayersTab = "Players"
+    $DK_ADPTab = "DK_ADP"
+    $DK_ExposureTab = "DK_Exposure"
+    $UD_ExposureTab = "UD_Exposure"
+    
+    $Players_data = Import-Csv -Path $KerraneRankings
+    $Players_data | Export-Excel -Path $ExcelFile -WorksheetName $PlayersTab -AutoSize
+    
+    $DK_ADP_data = Import-Csv -Path $DK_Adp
+    $DK_ADP_data | Export-Excel -Path $ExcelFile -WorksheetName $DK_ADPTab -AutoSize
+    
+    #$DK_Exposure_data = Import-Csv -Path $DK_Exposure
+    #$DK_Exposure_data | Export-Excel -Path $ExcelFile -WorksheetName $DK_ExposureTab -AutoSize
+    
+    $UD_Exposure_data = Import-Csv -Path $UD_Exposure
+    $UD_Exposure_data | Export-Excel -Path $ExcelFile -WorksheetName $UD_ExposureTab -AutoSize
+
+    Pop-Location
+}
+$UD_Exposure = Get-UDExposure -BBDir $BBDir -UD_csv $UD_csv
+#DK_Exposure = Get-DKExpsoure -BBDir $BBDir -DK_csv $DK_csv
+Add-Files -BBDir $BBDir -UD_Exposure $UD_Exposure
