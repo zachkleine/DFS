a9f01749450a444180331bd24140bbae4a9b0abd
Zachary Kleine <zach.kleine18@gmail.com>
2023-06-02 09:36:44 -0400

    fixed exposure output

--- bbexposure.ps1
+++ bbexposure.ps1
@@ -2,18 +2,21 @@ Function Get-UDExposure {
     Param(
         [CmdletBinding()]
         [Parameter(Mandatory=$False)][String]$BBDir = "G:\My Drive\Fantasy Football\BestBall",
-        [Parameter(Mandatory=$False)][String]$UD_csv = "UD_Exposure.csv"
+        [Parameter(Mandatory=$False)][String]$UD_csv = "Underdog_Exposure.csv"
     )
+    Push-Location -Path $BBDir
     $FilePath = Join-Path -Path $BBDir -ChildPath $UD_csv
-    $ExposureCsv = Import-Csv -Path $FilePath | Select-Object -Property "First Name", "Last Name", "Draft", "Name", "Exposure"
+    $ExposureCsv = Import-Csv -Path $FilePath | Select-Object -Property "First Name", "Last Name", "Draft", "Name"
     $DraftCount = ($ExposureCsv.Draft | Get-Unique).Count
     foreach ($Line in $ExposureCsv) {
         $Line."Name" = $Line."First Name"+" "+$Line."Last Name"
-        $Count = $ExposureCsv."Name" | Where-Object {$_ -eq $Line."Name"} | Measure-Object | Select-Object -ExpandProperty Count
-        $Exposure = (($Count/$DraftCount) * 100).ToString("N2") + "%"
-        $Line."Exposure" = $Exposure
     }
-    $ExposureCsv | Sort-Object -Property "Name" | Get-Unique
-    Return $ExposureCsv
+    $PlayerCounts = $ExposureCsv."Name" | Group-Object | Select-Object Name, Count, Exposure
+    foreach ($Player in $PlayerCounts) {
+        $Exposure = (($Player.Count/$DraftCount) * 100).ToString("N2") + "%"
+        $Player.Exposure = $Exposure
+    }
+    $PlayerCounts | Select-Object -Property Name,Exposure | Export-Csv -Path "UD_Exposure.csv" -NoTypeInformation -Force
+    Pop-Location
 }
 $Csv = Get-UDExposure
