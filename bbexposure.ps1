c6ed45bf0a916c3b2aef91859672bd95231f9c91
Zachary Kleine <zach.kleine18@gmail.com>
2023-06-04 07:31:39 -0400

    added DK rankings support

--- bbexposure.ps1
+++ bbexposure.ps1
@@ -32,23 +32,29 @@ Function Add-Files {
         [Parameter(Mandatory=$False)][String]$BBDir
     )
     $ExcelFile = "BestBallBuddy.xlsx"
-    $KerraneRankings = "Best-Ball---Ranks.csv"
-    $DK_Adp = "DkPreDraftRankings.csv"
+    $UDRankings = "Best-Ball---UD-Ranks.csv"
+    $DKRankings = "Best-Ball---DK-Ranks.csv"
     #$DK_Exposure = "DK_Exposure.csv"
     $UD_Exposure = "UD_Exposure.csv"
+
     Push-Location -Path $BBDir
-    $PlayersTab = "Players"
+    $UD_ADPTab = "UD_ADP"
     $DK_ADPTab = "DK_ADP"
     #$DK_ExposureTab = "DK_Exposure"
     $UD_ExposureTab = "UD_Exposure"
-    $Players_data = Import-Csv -Path $KerraneRankings
-    $Players_data | Export-Excel -Path $ExcelFile -WorksheetName $PlayersTab -AutoSize
-    $DK_ADP_data = Import-Csv -Path $DK_Adp
-    $DK_ADP_data | Export-Excel -Path $ExcelFile -WorksheetName $DK_ADPTab -AutoSize
+    
+    $UD_ADP_data = Import-Csv -Path $UDRankings
+    $UD_ADP_data | Export-Excel -Path $ExcelFile -WorksheetName $UD_ADPTab
+    
+    $DK_ADP_data = Import-Csv -Path $DKRankings
+    $DK_ADP_data | Export-Excel -Path $ExcelFile -WorksheetName $DK_ADPTab
+    
     #$DK_Exposure_data = Import-Csv -Path $DK_Exposure
-    #$DK_Exposure_data | Export-Excel -Path $ExcelFile -WorksheetName $DK_ExposureTab -AutoSize
+    #$DK_Exposure_data | Export-Excel -Path $ExcelFile -WorksheetName $DK_ExposureTab
+    
     $UD_Exposure_data = Import-Csv -Path $UD_Exposure
-    $UD_Exposure_data | Export-Excel -Path $ExcelFile -WorksheetName $UD_ExposureTab -AutoSize
+    $UD_Exposure_data | Export-Excel -Path $ExcelFile -WorksheetName $UD_ExposureTab
+    
     Pop-Location
 }
 $UD_Exposure = Get-UDExposure -BBDir $BBDir -UD_csv $UD_csv
