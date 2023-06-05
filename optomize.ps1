c795dc399e5021ecb6905d7628ab2e3c24857645
Zachary Kleine <zach.kleine18@gmail.com>
2023-06-05 14:17:16 -0400

    refactored to create function for dksalaries

--- optomize.ps1
+++ optomize.ps1
@@ -1,18 +1,27 @@
 Param(
-    [Parameter(Mandatory=$False)]$DfsDir = "C:\Users\zachk\Downloads", #G:\My Drive\Fantasy Football\DFS\2022\
-    [Parameter(Mandatory=$False)]$Week = "Test" #14
+    [CmdletBinding()]
+    [Parameter(Mandatory=$False)][String]$DfsDir = "G:\My Drive\Fantasy Football\DFS\2022",
+    [Parameter(Mandatory=$False)][int]$Week = "13"
 )
-$FullDir = $DfsDir+"\Week$Week"
-$DKCsv = Import-Csv -Path $FullDir"\DKSalaries.csv"
-$ETRCsv = Import-Csv -Path $FullDir"\ETRProj.csv"
-foreach ($row in $DKCsv) {
-    $name = $row.Name
-    $lookup = $ETRCsv `
-        | Where-Object {$_.Name -eq $name} `
-        | Select-Object -ExpandProperty 'DK Projection'
-    $row.AvgPointsPerGame = $lookup
+Function Get-DKSalaries {
+    Param(
+        [CmdletBinding()]
+        [Parameter(Mandatory=$False)][String]$DfsDir,
+        [Parameter(Mandatory=$False)][int]$Week
+    )
+    $FullDir = $DfsDir+"\Week$Week"
+    $DKCsv = Import-Csv -Path $FullDir"\DKSalaries.csv"
+    $ETRCsv = Import-Csv -Path $FullDir"\ETRProj.csv"
+    foreach ($row in $DKCsv) {
+        $name = $row.Name
+        $lookup = $ETRCsv `
+            | Where-Object {$_.Name -eq $name} `
+            | Select-Object -ExpandProperty 'DK Projection'
+        $row.AvgPointsPerGame = $lookup
+    }
+    $DKCsv | Export-Csv -Path $FullDir"\DKSalaries.csv" -NoTypeInformation -Force
 }
-$DKCsv | Export-Csv -Path $FullDir"\DKSalaries.csv" -NoTypeInformation -Force
+Get-DKSalaries -DfsDir $DfsDir -Week $Week
 
 <#  Need to call PyDFS program
 from pydfs_lineup_optimizer import get_optimizer, Site, Sport, CSVLineupExporter
