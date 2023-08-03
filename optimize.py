faba5199971a68eb48e4e5d6361eaa36f020e161
Zachary Kleine <zach.kleine18@gmail.com>
2023-08-03 16:03:58 -0400

    added next steps and parameter prompt

--- optimize.py
+++ optimize.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def get_dk_salaries(dfs_dir="G:\\My Drive\\Fantasy Football\\DFS\\2023", week=13):
+def get_dk_salaries(dfs_dir, week):
     full_dir = f"{dfs_dir}\\Week{week}"
     dk_csv_path = f"{full_dir}\\DKSalaries.csv"
     etr_csv_path = f"{full_dir}\\ETRProj.csv"
@@ -17,5 +17,16 @@ def get_dk_salaries(dfs_dir="G:\\My Drive\\Fantasy Football\\DFS\\2023", week=13
     dk_csv.to_csv(dk_csv_path, index=False)
 
 dfs_dir = "G:\\My Drive\\Fantasy Football\\DFS\\2023"
-week = 13
+week = int(input("Enter Week #"))
 get_dk_salaries(dfs_dir, week)
+
+"""
+Need to call PyDFS program
+from pydfs_lineup_optimizer import get_optimizer, Site, Sport, CSVLineupExporter
+
+DKoptimizer = get_optimizer(Site.DRAFTKINGS, Sport.FOOTBALL)
+DKoptimizer.load_players_from_csv('DKSalaries.csv')
+
+lineups = list(DKoptimizer.optimize(10))
+DKoptimizer.export('result.csv')
+"""
