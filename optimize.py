9725b1d32a64c5dda569181d146c428149a935c5
Zachary Kleine <zach.kleine18@gmail.com>
2023-08-09 14:19:47 -0400

    added ownership and ceiling headers to csv

--- optimize.py
+++ optimize.py
@@ -34,7 +34,23 @@ def get_dk_opto(dfs_dir, week):
     dk_csv = dk_csv.applymap(remove_parentheses)    
     dk_csv.to_csv(results_csv_path, index=False)
 
+def get_dk_extras(dfs_dir, week): 
+    full_dir = f"{dfs_dir}\\Week{week}"
+    dk_opto_csv = f"{full_dir}\\dkcashopto.csv"
+    etr_csv_path = f"{full_dir}\\ETRProj.csv"
+
+    etr_csv = pd.read_csv(etr_csv_path)
+    dk_opto = pd.read_csv(dk_opto_csv)
+
+    additional_headers = ['Ownership', 'Ceiling']
+    new_headers_df = pd.DataFrame(columns=additional_headers)
+    df_with_headers = pd.concat([dk_opto, new_headers_df], axis=1)
+    df_with_headers.to_csv(dk_opto_csv, index=False)
+    
+
+
 dfs_dir = "G:\\My Drive\\Fantasy Football\\DFS\\2023"
 week = "13"
 get_dk_salaries(dfs_dir, week)
 get_dk_opto(dfs_dir, week)
+get_dk_extras(dfs_dir, week)
