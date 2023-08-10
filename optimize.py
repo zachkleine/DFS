53ac07350ffc4f1063bd53d9e8acf5980b61722e
Zachary Kleine <zach.kleine18@gmail.com>
2023-08-10 16:00:10 -0400

    set avg ppg to 0 if no match in projections

--- optimize.py
+++ optimize.py
@@ -14,6 +14,8 @@ def get_dk_salaries(dfs_dir, week):
         lookup = etr_csv.loc[etr_csv['Name'] == name, 'DK Projection']
         if not lookup.empty:
             dk_csv.at[_, 'AvgPointsPerGame'] = lookup.iloc[0]
+        else: 
+            dk_csv.at[_, 'AvgPointsPerGame'] = 0
 
     dk_csv.to_csv(dk_csv_path, index=False)
 
@@ -25,7 +27,7 @@ def get_dk_opto(dfs_dir, week):
     from pydfs_lineup_optimizer import get_optimizer, Site, Sport, CSVLineupExporter
     DKOptimizer = get_optimizer(Site.DRAFTKINGS, Sport.FOOTBALL)
     DKOptimizer.load_players_from_csv(dk_csv_path)
-    lineups = list(DKOptimizer.optimize(2))
+    lineups = list(DKOptimizer.optimize(100))
     DKOptimizer.export(results_csv_path)
 
     dk_csv = pd.read_csv(results_csv_path)
@@ -48,7 +50,7 @@ def get_dk_extras(dfs_dir, week):
     df_with_headers.to_csv(dk_opto_csv, index=False)
 
 dfs_dir = "G:\\My Drive\\Fantasy Football\\DFS\\2023"
-week = "13"
+week = "15"
 get_dk_salaries(dfs_dir, week)
 get_dk_opto(dfs_dir, week)
 get_dk_extras(dfs_dir, week)
