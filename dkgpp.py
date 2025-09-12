fc124f75e4922b48bc77e08998425e272c18ff59
Zachary Kleine <zach.kleine18@gmail.com>
2025-09-12 13:46:27 -0400

    updated dk gpp rules

--- dkgpp.py
+++ dkgpp.py
@@ -69,47 +69,14 @@ def get_dk_opto(dk_csv_path, results_csv_path):
     # Chalk = PlayersGroup(DKOptimizer.player_pool.get_players(''),max_from_group=3)
     # DKOptimizer.add_players_group(Chalk)
 
-    Leverage = PlayersGroup(DKOptimizer.player_pool.get_players("Darnell Mooney", "Stefon Diggs", "Sam LaPorta", "Amon-Ra St. Brown", "Jameson Williams", "Marvin Harrison Jr.", "Travis Hunter", "Deebo Samuel Sr.", "Jaylen Waddle",
-                                                                "Chris Olave", "Ashton Jeanty", "Kyren Williams", "Matthew Golden", "Michael Pittman Jr.", "Jakobi Meyers", "Romeo Doubs", "Rashid Shaheed"),
-                                                                min_from_group=1)    
+    Leverage = PlayersGroup(DKOptimizer.player_pool.get_players(''),min_from_group=1)    
     DKOptimizer.add_players_group(Leverage)
-
-    CINDouble = PlayersGroup(DKOptimizer.player_pool.get_players('Joe Burrow','Chase Brown','Jamarr Chase','Tee Higgins'),
-                         max_from_group=3,min_from_group=3,
-                         depends_on=DKOptimizer.player_pool.get_player_by_name('Joe Burrow'),strict_depend=False)
-    INDSingle = PlayersGroup(DKOptimizer.player_pool.get_players('Daniel Jones','Michael Pittman','Josh Downs'),
-                         max_from_group=2,min_from_group=2,
-                         depends_on=DKOptimizer.player_pool.get_player_by_name('Daniel Jones'),strict_depend=False)
-    MIABB = PlayersGroup(DKOptimizer.player_pool.get_players('Daniel Jones','Tyreek Hill','Devon Achane','Jaylen Waddle'),
-                    max_from_group=3,min_from_group=3,
-                    depends_on=DKOptimizer.player_pool.get_player_by_name('Daniel Jones'),strict_depend=False)
-    WASStack = PlayersGroup(DKOptimizer.player_pool.get_players('Jayden Daniels','Deebo Samuel','Malik Nabers'),
-                    max_from_group=3,min_from_group=3,
-                    depends_on=DKOptimizer.player_pool.get_player_by_name('Jayden Daniels'),strict_depend=False)
-    MIADouble = PlayersGroup(DKOptimizer.player_pool.get_players('Tua Tagovailoa','Tyreek Hill','Devon Achane','Jaylen Waddle'),
-                    max_from_group=3,min_from_group=3,
-                    depends_on=DKOptimizer.player_pool.get_player_by_name('Tua Tagovailoa'),strict_depend=False)
-    INDBB = PlayersGroup(DKOptimizer.player_pool.get_players('Tua Tagovailoa','Jonathan Taylor','Michael Pittman','Josh Downs'),
-                        max_from_group=2,min_from_group=2,
-                        depends_on=DKOptimizer.player_pool.get_player_by_name('Tua Tagovailoa'),strict_depend=False)
-    ATLDouble = PlayersGroup(DKOptimizer.player_pool.get_players('Michael Penix Jr.','Drake London','Ray-Ray McCloud III','Darnell Mooney'),
-                    max_from_group=3,min_from_group=3,
-                    depends_on=DKOptimizer.player_pool.get_player_by_name('Michael Penix Jr.'),strict_depend=False)
-    TBBB = PlayersGroup(DKOptimizer.player_pool.get_players('Michael Penix Jr.','Mike Evans','Bucky Irving','Emeka Egbuka'),
-                    max_from_group=3,min_from_group=2,
-                    depends_on=DKOptimizer.player_pool.get_player_by_name('Michael Penix Jr.'),strict_depend=False)
-    JAXDouble = PlayersGroup(DKOptimizer.player_pool.get_players('Trevor Lawrence','Brian Thomas Jr.','Brenton Strange','Travis Hunter','Tetairoa McMillan'),
-                    max_from_group=3,min_from_group=3,
-                    depends_on=DKOptimizer.player_pool.get_player_by_name('Trevor Lawrence'),strict_depend=False)
-    SFDouble = PlayersGroup(DKOptimizer.player_pool.get_players('Brock Purdy','George Kittle','Ricky Pearsall'),
-                    max_from_group=3,min_from_group=3,
-                    depends_on=DKOptimizer.player_pool.get_player_by_name('Brock Purdy'),strict_depend=False)
 
     # #LimitStackExposure = DKOptimizer.player_pool.get_player_by_name([''])
     # #LimitStackExposure.max_exposure = 0.01
 
-    DKOptimizer.add_stack(Stack([CINDouble,INDSingle,MIABB,WASStack,MIADouble,INDBB,ATLDouble,TBBB,JAXDouble,SFDouble]))
-    DKOptimizer.add_stack(PositionsStack(['QB',('WR','TE')],for_teams=['SF','NE','ATL','WAS','JAX','IND','MIA','CIN'],max_exposure=0.13))
+    DKOptimizer.add_stack(Stack([]))
+    DKOptimizer.add_stack(PositionsStack(['QB',('WR','TE')],for_teams=['']))
     # DKOptimizer.force_positions_for_opposing_team(('QB','WR'))
 
     ## END RULES
